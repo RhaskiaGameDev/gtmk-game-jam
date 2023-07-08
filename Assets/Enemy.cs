8c48df962166ae3480ab42b37f6a906a6ee56436
Rhaskia <94507929+RhaskiaGameDev@users.noreply.github.com>
2023-07-08 14:58:19 +1200

    yes

---
+++ Assets/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float accel;
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SwitchManager.Instance.currentEnemy != this) return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        Vector2 velocity = rb.velocity;
+
+        velocity.x += accel * Input.GetAxis("Horizontal") * Time.deltaTime;
+        velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+
+        rb.velocity = velocity;
+    }
+}
--- Assets/SwitchManager.cs
+++ Assets/SwitchManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SwitchManager : MonoBehaviour
 {
+    public static SwitchManager Instance;
+    public Enemy currentEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
     }
 
     // Update is called once per frame
